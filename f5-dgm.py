51dcdcbc2352f3093fd05f4d901eacb6bdb24e4c
Greg Robinson <g.robinson@f5.com>
2024-08-02 11:01:10 -0400

    Added function to ignore built-in data groups when importing from BIG-IP

--- f5-dgm.py
+++ f5-dgm.py
@@ -39,10 +39,12 @@ app = Flask(__name__)
 # Enforce HTTPS
 talisman = Talisman(app)
 
+# Create app security policy
 csp = {
     'default-src': ['\'self\'', 'https://*', '\'unsafe-inline\'',  '\'unsafe-eval\''],
     'script-src': ['\'self\'', 'https://*', '\'unsafe-inline\'',  '\'unsafe-eval\'', '*']
 }
+
 # HTTP Strict Transport Security (HSTS) Header
 hsts = {
 	'max-age': 31536000,
@@ -59,6 +61,14 @@ talisman.session_cookie_samesite = 'Lax'
 # Add the headers to Talisman
 talisman.content_security_policy = csp
 talisman.strict_transport_security = hsts
+
+# Tell the client browsers to not cache any pages
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 # Load secret key from file with error handling
 try:
@@ -88,11 +98,174 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
 DEVICES_FILE = 'devices.json'
 DATAGROUPS_FILE = 'datagroups.json'
 
+# Define built-in data groups for TMOS
+TMOS_BUILT_IN_DATA_GROUPS = [
+    {
+      "name": "aol",
+      "partition": "Common",
+      "type": "ip",
+      "records": [
+        {
+          "name": "64.12.96.0/19",
+          "data": ""
+        },
+        {
+          "name": "195.93.16.0/20",
+          "data": ""
+        },
+        {
+          "name": "195.93.48.0/22",
+          "data": ""
+        },
+        {
+          "name": "195.93.64.0/19",
+          "data": ""
+        },
+        {
+          "name": "195.93.96.0/19",
+          "data": ""
+        },
+        {
+          "name": "198.81.0.0/22",
+          "data": ""
+        },
+        {
+          "name": "198.81.8.0/23",
+          "data": ""
+        },
+        {
+          "name": "198.81.16.0/20",
+          "data": ""
+        },
+        {
+          "name": "202.67.65.128/25",
+          "data": ""
+        },
+        {
+          "name": "205.188.112.0/20",
+          "data": ""
+        },
+        {
+          "name": "205.188.146.144/30",
+          "data": ""
+        },
+        {
+          "name": "205.188.192.0/20",
+          "data": ""
+        },
+        {
+          "name": "205.188.208.0/23",
+          "data": ""
+        },
+        {
+          "name": "207.200.112.0/21",
+          "data": ""
+        }
+      ]
+    },
+    {
+      "name": "images",
+      "partition": "Common",
+      "type": "string",
+      "records": [
+        {
+          "name": ".bmp",
+          "data": ""
+        },
+        {
+          "name": ".gif",
+          "data": ""
+        },
+        {
+          "name": ".jpg",
+          "data": ""
+        }
+      ]
+    },
+    {
+      "name": "private_net",
+      "partition": "Common",
+      "type": "ip",
+      "records": [
+        {
+          "name": "10.0.0.0/8",
+          "data": ""
+        },
+        {
+          "name": "172.16.0.0/12",
+          "data": ""
+        },
+        {
+          "name": "192.168.0.0/16",
+          "data": ""
+        }
+      ]
+    },
+    {
+      "name": "sys_APM_MS_Office_OFBA_DG",
+      "partition": "Common",
+      "description": "This internal data-group is used in _sys_APM_MS_Office_OFBA_Support irule",
+      "type": "string",
+      "records": [
+        {
+          "name": "ie_sp_session_sharing_enabled",
+          "data": "0"
+        },
+        {
+          "name": "ie_sp_session_sharing_inactivity_timeout",
+          "data": "60"
+        },
+        {
+          "name": "ofba_auth_dialog_size",
+          "data": "800x600"
+        },
+        {
+          "name": "useragent1",
+          "data": "microsoft data access internet publishing provider"
+        },
+        {
+          "name": "useragent2",
+          "data": "office protocol discovery"
+        },
+        {
+          "name": "useragent3",
+          "data": "microsoft office"
+        },
+        {
+          "name": "useragent4",
+          "data": "non-browser"
+        },
+        {
+          "name": "useragent5",
+          "data": "msoffice 12"
+        },
+        {
+          "name": "useragent6",
+          "data": "microsoft-webdav-miniredir"
+        },
+        {
+          "name": "useragent7",
+          "data": "webdav-miniredir"
+        },
+        {
+          "name": "useragent9",
+          "data": "ms frontpage 1[23456789]"
+        },
+        {
+          "name": "useragent10",
+          "data": "onenote"
+        }
+      ]
+    }
+  ]
+
+
 # Ensure the JSON files exist
 for filename in [DEVICES_FILE, DATAGROUPS_FILE]:
     if not os.path.exists(filename):
         with open(filename, 'w') as f:
             json.dump([], f)
+
 
 # App route for the root page which displays all datagroups
 @app.route('/')
@@ -417,15 +590,18 @@ def import_from_bigips():
             device_datagroups.append(device)
 
     if request.method == 'POST':
+        ignore_builtin = request.form.get('ignore_builtin') == 'on'
         selected_datagroups = {}
         for device in device_datagroups:
             datagroups = request.form.getlist(f'datagroups_{device["name"]}')
             for datagroup in datagroups:
+                if ignore_builtin and is_builtin_datagroup(datagroup):
+                    continue
                 if datagroup not in selected_datagroups:
                     selected_datagroups[datagroup] = device
                 else:
                     flash(f'Duplicate data group name "{datagroup}" found across devices. Please ensure unique data group names.')
-                    return render_template('import_from_bigips.html', devices=device_datagroups)
+                    return render_template('import_from_bigips.html', devices=device_datagroups, TMOS_BUILT_IN_DATA_GROUPS=TMOS_BUILT_IN_DATA_GROUPS)
 
         success_count = 0
         failure_count = 0
@@ -439,7 +615,13 @@ def import_from_bigips():
         flash(f'Successfully imported {success_count} data groups. Failed to import {failure_count} data groups.')
         return redirect(url_for('index'))
 
-    return render_template('import_from_bigips.html', devices=device_datagroups)
+    return render_template('import_from_bigips.html', devices=device_datagroups, TMOS_BUILT_IN_DATA_GROUPS=TMOS_BUILT_IN_DATA_GROUPS)
+
+def is_builtin_datagroup(datagroup_name):
+    for dg in TMOS_BUILT_IN_DATA_GROUPS:
+        if dg['name'] == datagroup_name:
+            return True
+    return False
 
 def fetch_datagroups_from_bigip(device):
     try:
